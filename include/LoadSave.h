049e9c71159133e00a2d023e98e3dea444c49d7f
LeonardoDiCarpaccio <59809554+LeonardoDiCarpaccio@users.noreply.github.com>
2021-05-07 00:41:43 +0200

    lucien escousse

--- include/LoadSave.h
+++ include/LoadSave.h
@@ -20,9 +20,32 @@
 #include "Multiplication.h"
 #include "Division.h"
 
+void Save(Expression *E){
+    std::ofstream savefile("saved.txt", std::ofstream::out);
+    savefile << E->getStringNpi();
+    savefile.close();
+    return;
+}
 
-void Save(Expression *E);
-Expression *Load(std::string path);
-
+Expression *Load(std::string path){
+    std::string line;
+    std::ifstream myfile (path);
+    Expression *e;
+     if (myfile.is_open())
+     {
+         while ( getline (myfile,line) )
+             {
+               std::cout << line << '\n';
+               std::istringstream iss(line);
+               std::vector<std::string> tokens{std::istream_iterator<std::string>{iss},
+                                         std::istream_iterator<std::string>{}};
+               e = ParseVector(tokens);
+               std::cout << e;
+             }
+             myfile.close();
+     }
+     else std::cout << "Unable to open file";
+     return e;
+}
 
 #endif // LOADSAVE_H
