934304a0580c59ca9401a782f745aecc7a4ed116
Dominik <dominikb94@gmail.com>
2021-05-09 20:47:07 +0200

    added parsevector to saveload.h

--- include/LoadSave.h
+++ include/LoadSave.h
@@ -20,32 +20,10 @@
 #include "Multiplication.h"
 #include "Division.h"
 
-void Save(Expression *E){
-    std::ofstream savefile("saved.txt", std::ofstream::out);
-    savefile << E->getStringNpi();
-    savefile.close();
-    return;
-}
 
-Expression *Load(std::string path){
-    std::string line;
-    std::ifstream myfile (path);
-    Expression *e;
-     if (myfile.is_open())
-     {
-         while ( getline (myfile,line) )
-             {
-               std::cout << line << '\n';
-               std::istringstream iss(line);
-               std::vector<std::string> tokens{std::istream_iterator<std::string>{iss},
-                                         std::istream_iterator<std::string>{}};
-               e = ParseVector(tokens);
-               std::cout << e;
-             }
-             myfile.close();
-     }
-     else std::cout << "Unable to open file";
-     return e;
-}
+void Save(Expression *E);
+Expression *Load(std::string path);
+Expression * ParseVector(std::vector<std::string> tokens);
+
 
 #endif // LOADSAVE_H
