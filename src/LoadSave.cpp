2b20ce53c464df8b332d6eb051f67511830440ca
LeonardoDiCarpaccio <59809554+LeonardoDiCarpaccio@users.noreply.github.com>
2021-05-09 18:26:30 +0200

    lucien

--- src/LoadSave.cpp
+++ src/LoadSave.cpp
@@ -1,10 +1,29 @@
 #include "include/LoadSave.h"
 
 void Save(Expression *E){
-
+    std::ofstream savefile("saved.txt", std::ofstream::out);
+    savefile << E->getStringNpi();
+    savefile.close();
+    return;
 }
 
 Expression *Load(std::string path){
-     return 0;
+    std::string line;
+    std::ifstream myfile (path);
+    Expression *e;
+     if (myfile.is_open())
+     {
+         while ( getline (myfile,line) )
+             {
+               std::cout << line << '\n';
+               std::istringstream iss(line);
+               std::vector<std::string> tokens{std::istream_iterator<std::string>{iss},
+                                         std::istream_iterator<std::string>{}};
+               e = ParseVector(tokens);
+               std::cout << e;
+             }
+             myfile.close();
+     }
+     else std::cout << "Unable to open file";
+     return e;
 }
-
