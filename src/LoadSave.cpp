a76d47e5dbab37c08451d65282a0476ce875da55
maxpvp31 <loubet.maxime31@gmail.com>
2021-05-09 18:29:05 +0200

    ezze

--- src/LoadSave.cpp
+++ src/LoadSave.cpp
@@ -27,3 +27,49 @@ Expression *Load(std::string path){
      else std::cout << "Unable to open file";
      return e;
 }
+
+
+Expression * ParseVector(std::vector<std::string> tokens){
+    std::stack<Expression *> expressionStack;
+    for(auto s: tokens){
+        if(s.at(0) == '('){
+
+        }else if(s.at(0) == ')'){
+
+        }else if(s == "+"){
+            Expression *gauche = expressionStack.top();
+            expressionStack.pop();
+            Expression *droite = expressionStack.top();
+            expressionStack.pop();
+            Addition * add = new Addition(gauche, droite);
+            expressionStack.push(add);
+        }else if(s == "-"){
+            Expression *gauche = expressionStack.top();
+            expressionStack.pop();
+            Expression *droite = expressionStack.top();
+            expressionStack.pop();
+            Soustraction * s = new Soustraction(gauche, droite);
+            expressionStack.push(s);
+        }else if(s == "*"){
+            Expression *gauche = expressionStack.top();
+            expressionStack.pop();
+            Expression *droite = expressionStack.top();
+            expressionStack.pop();
+            Multiplication * m = new Multiplication(gauche, droite);
+            expressionStack.push(m);
+        }else if(s == "/"){
+            Expression *gauche = expressionStack.top();
+            expressionStack.pop();
+            Expression *droite = expressionStack.top();
+            expressionStack.pop();
+            Division * d = new Division(gauche, droite);
+            expressionStack.push(d);
+        }else{
+            float val = stof(s);
+            Constante *con = new Constante(val);
+            expressionStack.push(con);
+        }
+    }
+    Expression *e = expressionStack.top();
+    return e;
+}
